d131d3e209559ce2ac916e6e909c55f84cf0815a
oinsio <oinsio.srv@gmail.com>
2021-10-03 12:09:20 +0300

    fix formatting

--- app/src/test/groovy/cto/fantask/StringTransformationBSFTest.groovy
+++ app/src/test/groovy/cto/fantask/StringTransformationBSFTest.groovy
@@ -5,6 +5,7 @@ import spock.lang.Specification
 class StringTransformationBSFTest extends Specification {
 
     def "Test Case 000"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase000.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -18,7 +19,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -37,6 +38,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 001"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase001.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -50,7 +52,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -69,6 +71,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 002"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase002.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -82,7 +85,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -100,6 +103,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 003"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase003.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -113,7 +117,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -130,6 +134,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 004"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase004.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -143,7 +148,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -161,6 +166,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 005"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase005.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -174,7 +180,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -191,6 +197,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 006"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase006.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -204,7 +211,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -222,6 +229,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 007"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase007.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -235,7 +243,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -258,6 +266,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 008"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase008.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -271,7 +280,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -290,6 +299,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 009"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase009.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -303,7 +313,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -321,6 +331,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 010"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase010.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -334,7 +345,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -357,6 +368,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 011"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase011.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -370,7 +382,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -391,6 +403,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 012"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase012.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -404,7 +417,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -425,6 +438,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 013"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase013.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -438,7 +452,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -458,6 +472,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 014"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase014.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -471,7 +486,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -488,6 +503,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 015"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase015.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -501,7 +517,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -521,6 +537,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 016"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase016.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -534,7 +551,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -556,6 +573,7 @@ class StringTransformationBSFTest extends Specification {
     }
 
     def "Test Case 017"() {
+
         setup:
             def inputData = new File(getClass().getResource('/TestCase017.txt').toURI())
             StringTransformationBSF.readInputData(inputData)
@@ -569,7 +587,7 @@ class StringTransformationBSFTest extends Specification {
             def output = StringTransformationBSF.wordSequence(StringTransformationBSF.start,
                 StringTransformationBSF.stop, StringTransformationBSF.dictionary)
             println("Worked " + (System.currentTimeMillis() - now) + " milliseconds.")
-            def outputLength= 0
+            def outputLength = 0
             for (def word : output) {
                 outputLength += word.length() + 1
             }
@@ -587,5 +605,4 @@ class StringTransformationBSFTest extends Specification {
             println('dae')
             outputLength == expectedOutputLength
     }
-
 }
